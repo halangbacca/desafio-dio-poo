f311c33715f3c1d6b925802d94d07581da9076b7
halangbacca <halanbacca@outlook.com>
2024-07-29 20:15:05 -0300

    Herança e polimorfismo

---
+++ src/domain/Conteudo.java
@@ -0,0 +1,26 @@
+package domain;
+
+public abstract class Conteudo {
+    private String titulo;
+    private String descricao;
+    protected final Double XP_PADRAO = 10.0;
+
+    public abstract double calcularXp();
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+}
